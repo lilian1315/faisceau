299c4c7bfb776ef48851f5abe19e46000bfad5d2
lilian1315 <241207981+lilian1315@users.noreply.github.com>
2026-03-28 21:31:33 +0100

    feat: migrate to vite+

--- tsdown.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'tsdown'
-
-export default defineConfig({
-  exports: true,
-  entry: 'index.ts',
-  platform: 'neutral',
-  dts: true,
-  unbundle: true,
-})
---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite-plus'
+
+export default defineConfig({
+  staged: {
+    "*": "vp check --fix"
+  },
+  pack: {
+    exports: true,
+    entry: 'index.ts',
+    platform: 'neutral',
+    dts: true,
+    unbundle: true,
+  },
+  lint: {
+    options: {
+      typeAware: true,
+      typeCheck: true,
+    },
+  },
+  fmt: {
+    semi: false,
+    singleQuote: true,
+  },
+})
