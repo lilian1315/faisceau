894a70f9a820e0ecf5c5f34182b6f6a0eb138b34
lilian1315 <241207981+lilian1315@users.noreply.github.com>
2026-03-29 03:16:45 +0200

    chore: remove useless package.json values

---
+++ index.test.ts
@@ -0,0 +1,54 @@
+import { it, expect, describe, vi } from 'vite-plus/test'
+import { CannotBecomeEqualError, signal, when, whenEqual } from './index.ts'
+
+describe('when', () => {
+  it('should resolve when the condition is true', async () => {
+    let promise = vi.fn(() => when(() => true))
+
+    void promise()
+    await sleep(1)
+    expect(promise).toHaveResolved()
+  })
+
+  it('should resolve when the condition become true', async () => {
+    let ready = signal(false)
+    let promise = vi.fn(() => when(() => ready.get() === true))
+
+    void promise()
+    await sleep(1)
+    expect(promise).not.toHaveResolved()
+    ready.set(true)
+    await sleep(1)
+    expect(promise).toHaveResolved()
+  })
+})
+
+describe('whenEqual', () => {
+  it('should resolve when not-reactive values are equal', async () => {
+    let promise = vi.fn(() => whenEqual('a' as any, 'a' as any))
+
+    void promise()
+    await sleep(1)
+    expect(promise).toHaveResolved()
+  })
+
+  it('should reject when not-reactive values are not equal', async () => {
+    await expect(whenEqual('a', 'b')).rejects.toThrow(CannotBecomeEqualError)
+  })
+
+  it('should resolve when the values become true', async () => {
+    let ready = signal(false)
+    let promise = vi.fn(() => whenEqual(ready, true))
+
+    void promise()
+    await sleep(1)
+    expect(promise).not.toHaveResolved()
+    ready.set(true)
+    await sleep(1)
+    expect(promise).toHaveResolved()
+  })
+})
+
+async function sleep(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms))
+}
